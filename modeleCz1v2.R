3aed3b4ae3ee1c5054ccfcd553c382ad60545cc6
rwozniak89 <rwozniak89@gmail.com>
2021-06-12 21:49:47 +0200

    changes

--- modeleCz1v2.R
+++ modeleCz1v2.R
@@ -4,51 +4,71 @@ summary(wszystkieOferty080621)
 str(wszystkieOferty080621)
 library(tidyverse)
 
-
 #install.packages("sjmisc")
 library(sjmisc)
-  str_contains(wszystkieOferty080621$cena, ",", logic=NULL)
-  str_contains(wszystkieOferty080621$`Pojemność skokowa`, ",", logic=NULL)
-  str_contains(wszystkieOferty080621$`Moc`, ",", logic=NULL)
-  str_contains(wszystkieOferty080621$`Rok produkcji`, ",", logic=NULL)
+str_contains(wszystkieOferty080621$cena,",",logic=NULL)
+str_contains(wszystkieOferty080621$`Pojemność skokowa`,",",logic=NULL)
+str_contains(wszystkieOferty080621$`Moc`,",",logic=NULL)
+str_contains(wszystkieOferty080621$`Rok produkcji`,",",logic=NULL)
 ?str_contains
-  
-#nie działa #as.numeric(wszystkieOferty080621$cena)
+
+#to nie dziala jakbysmy chcieli:as.numeric(wszystkieOferty080621$cena)
+
 names(wszystkieOferty080621)<-gsub(" ","_",names(wszystkieOferty080621))
 summary(wszystkieOferty080621)
+names(wszystkieOferty)
+nrow(wszystkieOferty)
 
 #mutate
-wszystkieOferty <- wszystkieOferty080621%>%mutate(cena = as.numeric( cena%>%str_replace_all("[^\\d,]", "") %>%str_replace_all(",",".")) )
-wszystkieOferty$cena
+wszystkieOferty <- wszystkieOferty080621%>%mutate(cena=   as.numeric(cena%>%str_replace_all("[^\\d,]", "")%>%str_replace_all(",",".")) ) 
 
+wszystkieOferty<- wszystkieOferty%>%mutate(Przebieg=as.numeric(Przebieg%>%str_replace_all("[^\\d,]","")%>%str_replace_all(",",".")))
+wszystkieOferty<- wszystkieOferty%>%mutate(Rok_produkcji=as.numeric(Rok_produkcji%>%str_replace_all("[^\\d,]","")%>%str_replace_all(",",".")))
+wszystkieOferty<- wszystkieOferty%>%mutate(Moc=as.numeric(Moc%>%str_replace_all("[^\\d,]","")%>%str_replace_all(",",".")))
 wszystkieOferty<-wszystkieOferty%>%mutate(Pojemność_skokowa= substr(Pojemność_skokowa,1,str_length(Pojemność_skokowa)-1) )
 wszystkieOferty<- wszystkieOferty%>%mutate(Pojemność_skokowa=as.numeric(Pojemność_skokowa%>%str_replace_all("[^\\d,]","")%>%str_replace_all(",",".")))
-#wszystkieOferty
-wszystkieOferty$Pojemność_skokowa
+#wszystkieOferty<- wszystkieOferty%>%mutate(Pojemność_skokowa=as.numeric(Pojemność_skokowa%>%str_replace_all("cm3","")%>%str_replace_all("[^\\d,]","")%>%str_replace_all(",",".")))
 
-#wszystkieOfertyTest<- wszystkieOferty%>%filter(waluta=="PLN", Bezwypadkowy=='Tak' | is.na(Bezwypadkowy) )
-wszystkieOferty<-wszystkieOferty%>%filter(Uszkodzony!="Tak" | is.na(Uszkodzony) )
+#wszystkieOfertyTest<- wszystkieOferty%>%filter(waluta=="PLN", (Bezwypadkowy=='Tak' | is.na(Bezwypadkowy))  )
+wszystkieOferty<-wszystkieOferty%>%filter(waluta=="PLN",Uszkodzony!="Tak" | is.na(Uszkodzony) )
+str(wszystkieOferty)
+summary(wszystkieOferty)
 
+nrow(wszystkieOferty)
 
 
 #select
 wszystkieOferty<-wszystkieOferty%>%select(-waluta,-Kategoria)
 wszystkieOferty<-wszystkieOferty%>%select(cena:Kolor,Stan)
 summary(wszystkieOferty)
-
+str(ofertyV)
 ofertyV<-wszystkieOferty%>%filter(Marka_pojazdu=='Volkswagen')
-
 
 #arrange
 #group_by
 #summarise
 wszystkieOferty%>%group_by(Marka_pojazdu)%>%summarise(n=n())%>%arrange(desc(n))
-
+str(ofertyV)
 summary(ofertyV)
 levels(ofertyV$Model_pojazdu)
 ofertyV<-ofertyV%>%select(-Marka_pojazdu)%>%droplevels()
+ofertyV<-ofertyV%>%select(-Liczba_miejsc,-Napęd)
+ofertyV<-na.omit(ofertyV)
+ofertyV%>%droplevels()
 
-install.packages(c("caTools","randomForest","rpart","e1071","kknn","caret","mlr"))
+View(mixed_assoc(ofertyV))
+#install.packages("VIM")
+library(VIM)
+aggr(ofertyV, numbers=TRUE, 
+     sortVars=TRUE, 
+     labels=names(data), 
+     cex.axis=.7)
+#install.packages("mice")
+library(mice)
+View(md.pattern(ofertyV))
+levels(ofertyV$Napęd)
+
+#install.packages(c("caTools","randomForest","rpart","e1071","kknn","caret","mlr"))
 library(caTools)
 library(randomForest)
 library(rpart)
@@ -61,8 +81,15 @@ set.seed(123)
 sample<-sample.split(Y=ofertyV,SplitRatio = .75)
 trains<-subset(ofertyV,sample==TRUE)
 tests<-subset(ofertyV,sample==FALSE)
+?randomForest
 
-#install.packages("rcompanion")
+regrRF<-randomForest(cena~.,data =trains )
+predictionsRF<-predict(regrRF,tests)
+myMAE<- mean( abs(tests$cena-predictionsRF) )
+
+mean(ofertyV$cena)
+
+
 require(rcompanion)
 # Calculate a pairwise association between all variables in a data-frame. In particular nominal vs nominal with Chi-square, numeric vs numeric with Pearson correlation, and nominal vs numeric with ANOVA.
 # Adopted from https://stackoverflow.com/a/52557631/590437
@@ -107,3 +134,14 @@ mixed_assoc = function(df, cor_method="spearman", adjust_cramersv_bias=TRUE){
   # apply function to each variable combination
   map2_df(df_comb$X1, df_comb$X2, f)
 }
+
+
+
+
+
+
+
+
+
+
+
